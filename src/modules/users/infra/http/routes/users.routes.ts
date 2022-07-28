514d86dfb3967d89e9c283bd6c050a3923569563
TiagoMLucio <tiagolucio100@gmail.com>
2022-07-27 22:37:49 -0300

    :construciton: refactor: shared middlewares folder

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import validateId from 'middlewares/validateId';
+import validateId from '@shared/infra/http/middlewares/validateId';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import UsersController from '../controllers/UsersController';
 import UserUpdateAvatarController from '../controllers/UserUpdateAvatarController';
